97ef44e2b839ace4f5bf5d89e89225ec6d4e8cf4
timalsinab <timalsinab@berea.edu>
2023-05-31 10:47:12 -0400

    added functionality to TDD.

--- servicable.py
+++ servicable.py
@@ -1,21 +1,6 @@
-from engine.capulet_engine import CapuletEngine
-from engine.sternman_engine import SternmanEngine
-from engine.willoughby_engine import WilloughbyEngine
-from battery.nubbin_battery import NubbinBattery
-from battery.spindler_battery import SpindlerBattery
-from car import Car
+from abc import ABC, abstractmethod
 
-class CarFactory:
-    @staticmethod
-    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
-        engine = CapuletEngine(current_mileage, last_service_mileage)
-        battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine, battery)
-        return car
-
-    @staticmethod
-    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
-        engine = WilloughbyEngine(current_mileage, last_service_mileage)
-        battery = SpindlerBattery(current_date, last_service_date)
-        car = Car(engine, battery)
-        return car
+class Serviceable(ABC):
+    @abstractmethod
+    def needs_service(self):
+        pass
